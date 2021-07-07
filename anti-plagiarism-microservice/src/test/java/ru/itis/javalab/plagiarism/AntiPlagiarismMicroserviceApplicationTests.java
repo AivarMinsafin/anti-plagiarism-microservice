f0b6d3059e5516cc7528cfe6a958857aa0829854
AivarMinsafin <min.aivar2001@gmail.com>
2021-07-07 17:25:45 +0300

    init project

---
+++ anti-plagiarism-microservice/src/test/java/ru/itis/javalab/plagiarism/AntiPlagiarismMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.javalab.plagiarism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiPlagiarismMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
